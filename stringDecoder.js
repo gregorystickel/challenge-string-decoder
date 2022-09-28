309e016aa8f728ed2e0d235b3820f338cdf358a4
Greg Stickel <gregory.stickel@live.com>
2022-09-28 09:15:18 -0400

    Update 9:15am

---
+++ stringDecoder.js
@@ -0,0 +1,21 @@
+
+const decoder = code => {
+    let splitStr = code.split('')
+    let result = ''
+  
+    for (let i = 0; i < splitStr.length; i++) {
+      if (!isNaN(+splitStr[i])) {
+        i += +splitStr[i]
+      } else {
+        result += splitStr[i]
+      }
+    }
+  
+    return result
+  }
+  
+  console.log(decoder(`0h2xce5ngbrdy`));
+
+  // decoder('0y4akjfe0s')
+  // decoder('0h2xce5ngbrdy')
+  // decoder('2bna0p1mp2osl0e')
